6dc275d53d6999ca6a7f829887c19d41304ee19c
naufalnabila <naufalnabila27@gmail.com>
2022-07-18 14:29:28 +0700

    benerin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -2,7 +2,43 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <h1 class="h3 mb-2 text-gray-800"><?= $title ?></h1>
+    <p class="mb-4">List user yang telah terdaftar di database</p>
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">User Table</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>ID</th>
+                            <th>Nama</th>
+                            <th>Email</th>
+                            <th>Tanggal Daftar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $i = 1; ?>
+                        <?php foreach ($users as $r) : ?>
+                            <tr>
+                                <th scope="row"><?= $i; ?></th>
+                                <td><?= $r['id']; ?></td>
+                                <td><?= $r['name']; ?></td>
+                                <td><?= $r['email']; ?></td>
+                                <td><?= date('d F Y', $r['date_created']); ?></td>
+                            </tr>
+                            <?php $i++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 </div>
 <!-- /.container-fluid -->
